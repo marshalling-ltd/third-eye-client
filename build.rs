4c63f708b5dd1c75391e990813d16a87b0d67803
Vjeran Kokanovic <vjeran.kokanovic@marshalling.eu>
2026-04-14 14:41:41 +0200

    adding mapbox features

--- build.rs
+++ build.rs
@@ -2,6 +2,13 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    // Load .env at compile time and re-export vars so env!() works in source.
+    if let Ok(iter) = dotenvy::dotenv_iter() {
+        for item in iter.flatten() {
+            println!("cargo:rustc-env={}={}", item.0, item.1);
+        }
+    }
+    println!("cargo:rerun-if-changed=.env");
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
     if target_os != "macos" {
         return;
